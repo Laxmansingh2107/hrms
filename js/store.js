d2d3b1bcdc6a408c054925b90a3ba93285e81ca8
Laxmansingh2107 <luckysingh0508.lsj@gmail.com>
2026-01-05 04:06:08 +0530

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,1 +1,17 @@
+const Store = {
+  getEmployees() {
+    return JSON.parse(localStorage.getItem("employees")) || [];
+  },
 
+  saveEmployees(data) {
+    localStorage.setItem("employees", JSON.stringify(data));
+  },
+
+  getAttendance() {
+    return JSON.parse(localStorage.getItem("attendance")) || [];
+  },
+
+  saveAttendance(data) {
+    localStorage.setItem("attendance", JSON.stringify(data));
+  }
+};
